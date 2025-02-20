85fd1e480c52b86c80b5bca5bc473a121b1408db
Cresson Jeremy <jrmcresson@gmail.com>
2025-02-20 08:36:21 +0100

    netlify

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,19 +1,18 @@
-import Footer from "../components/footer";
+import Footer from "../components/Footer";
+import ClientLayout from "../components/ClientLayout";
 import "../../styles/global.css";
-import ClientLayout from "../components/clientlayout";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="fr">
       <head>
         <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>Mon Portfolio</title>
-        <link rel="icon" href="/logoJC-2.png" />
+        <link rel="icon" href="/logoJC-2.png" sizes="any" />
       </head>
       <body>
-        <ClientLayout>
-          {children}
-        </ClientLayout>
+        <ClientLayout>{children}</ClientLayout>
         <Footer />
       </body>
     </html>
