30ffc3955f9145f288b131d6a406b6e2c5b71996
Cresson Jeremy <jrmcresson@gmail.com>
2025-02-21 17:17:02 +0100

    upgarde site

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -2,7 +2,6 @@
 
 import { useState } from "react";
 import { motion } from "framer-motion";
-import Modal from "../../components/modal";
 import styles from "./contact.module.css";
 
 export default function Contact() {
@@ -88,12 +87,6 @@ export default function Contact() {
           {loading ? "Envoi en cours..." : "Envoyer"}
         </motion.button>
       </motion.form>
-
-      <Modal
-        isVisible={isModalVisible}
-        onClose={() => setModalVisible(false)}
-        message={modalMessage}
-      />
     </div>
   );
 }
